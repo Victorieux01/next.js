32a98bb12175fab85919165f895627b9bb291473
Victorieux01 <victordumas01@gmail.com>
2026-03-09 13:59:39 -0400

    page

--- app/page.tsx
+++ app/page.tsx
@@ -20,11 +20,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
     >
-            <strong>Welcome to Coredon.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Coredon.</strong> This is the our project!
           </p>
           <Link
             href="/login"
