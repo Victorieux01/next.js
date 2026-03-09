8ae78850d0de57ff518c33df1a5124ee31f34e6b
Victorieux01 <victordumas01@gmail.com>
2026-03-08 20:18:51 -0400

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | Coredon Dashboard',
+    default: 'Coredon Dashboard',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
