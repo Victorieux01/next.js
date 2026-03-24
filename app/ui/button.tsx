fc79e01549cfe5b391227caa878f876b6511452d
Victorieux01 <victordumas01@gmail.com>
2026-03-24 10:34:40 -0400

    button fix

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -7,15 +7,20 @@ import { requestInvoiceDeletion } from '@/app/lib/actions';
 import DeleteConfirmationModal from '@/app/ui/invoices/delete_confirmation_modal';
 import { formatCurrency } from '@/app/lib/utils';
 
-export function Button({
-  children,
-  className,
-  ...rest
-}: React.ComponentPropsWithoutRef<'button'>) {
+import clsx from 'clsx';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export function Button({ children, className, ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
-      className={`flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50 ${className ?? ''}`}
+      className={clsx(
+        'flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+        className,
+      )}
     >
       {children}
     </button>
